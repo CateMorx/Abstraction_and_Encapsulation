2ff2a8f194497a47ec0aae4da1a3e11023bffa73
CateMorx <morco.catelynjoy.12192003@gmail.com>
2023-07-01 13:37:10 +0800

    def for assigning values

--- Test_Fan.py
+++ Test_Fan.py
@@ -10,5 +10,8 @@ from tkinter import Tk
 #Class TestFan
 class Test_Fan:
 #def for assigning values for fan 1 & 2
+    def main(self):
+        fan1 = Fan(speed=Fan.FAST, radius=10, color='yellow', on=True)
+        fan2 = Fan(speed=Fan.MEDIUM, radius=5, color='blue', on=False)
 #def GUI
 #starts the event loop of the GUI application
