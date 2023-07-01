7a7ce56a227ed9809b1da8cfe34b450069488415
CateMorx <morco.catelynjoy.12192003@gmail.com>
2023-07-01 22:11:18 +0800

    additional comments

--- Car_Object.py
+++ Car_Object.py
@@ -12,13 +12,17 @@ from tkinter import Tk, Text
 class Car_Object:
     #GUI constructor
     def __init__(self):
+        #creates window for GUI
         self.root = Tk()
         self.root.title("Speed Display")
 
+        #creates text widget for GUI
         self.text = Text(self.root)
         self.text.pack()
 
+    #def that shows output in GUI
     def show_output(self, output):
+        #Appends each item in output list to be shown in text widget
         for item in output:
             self.text.insert("end", str(item) + "\n")
 
@@ -26,19 +30,29 @@ class Car_Object:
     def main(self):
          # Create a Car object
         car = Car(2023, "Tesla")
+
+        #instantiates list for output
         output = []
 
         #accelerates and display speed 5 times
         for _ in range(5):
+            #calls accelerate function
             car.accelerate()
+            #appends lines of text output into output list
             output.append("Car accelerates \n"+"Current speed:"+ str(car.get_speed()))
             
         #decelerates and display speed 5 times
         for _ in range(5):
+            #calls brake function
             car.brake()
+            #appends lines of text output into output list
             output.append("Car decelerates using brakes \n"+"Current speed:"+ str(car.get_speed()))
+
+        #calls function tha shows output
         self.show_output(output)
+        #starts the event loop of the GUI application
         self.root.mainloop()
+
     #def that creates rainbow window background
 
 #Executes code within main
