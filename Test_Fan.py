29a102c3c590ccfcf09471540758eab614e76c21
CateMorx <morco.catelynjoy.12192003@gmail.com>
2023-07-01 13:29:04 +0800

    New python file for testfan class

---
+++ Test_Fan.py
@@ -0,0 +1,9 @@
+#Created by: Catelyn Joy M.Morco from BSCPE 1-4
+#Assignment 9
+# creates two Fan objects. For the first object, assign the maximum speed, radius 10, color yellow, and turn it on.
+# Assign medium speed, radius 5, color blue, and turn it off for the second object. Display each object’s speed, radius, color, and on properties.
+
+#Class TestFan
+#def for assigning values for fan 1 & 2
+#def GUI
+#starts the event loop of the GUI application
