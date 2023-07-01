f96ea1b9b5b455ae2677f8f4a48e1d365f3d5912
CateMorx <morco.catelynjoy.12192003@gmail.com>
2023-07-01 21:25:11 +0800

    creates car object class, and its constructor for GUI

--- Car_Object.py
+++ Car_Object.py
@@ -4,9 +4,16 @@
 #get the current speed of the car and display it. Then call the brake method five times. After each call to the brake method, 
 #get the current speed of the car and display it.
 
-#Class for Car_Object
+#imports necessary elements
+from Car_Class import Car
+from tkinter import Tk, Label
 
+#Class for Car_Object
+class Car_Object:
     #GUI constructor
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("Output Window")
 
     #def create car object
         #accelerates and display speed 5 times
