924ab2d8155b63df5bc9c607429cb7f7c03938e3
CateMorx <morco.catelynjoy.12192003@gmail.com>
2023-07-02 01:16:37 +0800

    fixed label, text box, and button widget for age and animal type

--- Pet_Object.py
+++ Pet_Object.py
@@ -23,21 +23,22 @@ class Pet_GUI:
         self.pet.name_button= tkinter.Button(GUI, text="Enter", command=self.name_enter)
         self.pet.name_button.pack(pady=10)
 
-        #Label, Text Box, and Button for name Input
-        self.pet.name_label = tkinter.Label(GUI, text="Please Enter the Name of Your Pet:")
-        self.pet.name_label.pack(pady=5)
-        self.pet.name_entry = tkinter.Entry(GUI)
-        self.pet.name_entry.pack(pady=5)
-        self.pet.name_button= tkinter.Button(GUI, text="Enter", command=self.name_enter)
-        self.pet.name_button.pack(pady=10)
+        #Label, Text Box, and Button for Animal Type Input
+        self.pet.animal_type_label = tkinter.Label(GUI, text="Please Enter the Type of Animal Your Pet Is:")
+        self.pet.animal_type_label.pack(pady=5)
+        self.pet.animal_type_entry = tkinter.Entry(GUI)
+        self.pet.animal_type_entry.pack(pady=5)
+        self.pet.animal_type_button= tkinter.Button(GUI, text="Enter", command=self.animal_type_enter)
+        self.pet.animal_type_button.pack(pady=10)
 
-        #Label, Text Box, and Button for name Input
-        self.pet.name_label = tkinter.Label(GUI, text="Please Enter the Name of Your Pet:")
-        self.pet.name_label.pack(pady=5)
-        self.pet.name_entry = tkinter.Entry(GUI)
-        self.pet.name_entry.pack(pady=5)
-        self.pet.name_button= tkinter.Button(GUI, text="Enter", command=self.name_enter)
-        self.pet.name_button.pack(pady=10)
+        #Label, Text Box, and Button for Age Input
+        self.pet.age_label = tkinter.Label(GUI, text="Please Enter the Age of Your Pet:")
+        self.pet.age_label.pack(pady=5)
+        self.pet.age_entry = tkinter.Entry(GUI)
+        self.pet.age_entry.pack(pady=5)
+        self.pet.age_button= tkinter.Button(GUI, text="Enter", command=self.age_enter)
+        self.pet.age_button.pack(pady=10)
+        
     # def for set_name()
     # def for set_animal_type()
     # def for set_age()
