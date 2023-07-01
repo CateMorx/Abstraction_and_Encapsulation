0991f8415d43a577ca0e9113f77ea990b7131936
CateMorx <morco.catelynjoy.12192003@gmail.com>
2023-07-02 00:54:44 +0800

    creates method for all setters

--- Pet_Class.py
+++ Pet_Class.py
@@ -11,9 +11,19 @@ class Pet:
         self.__name = ""
         self.__animal_type = ""
         self.__age = ""
+
     # def set_name()
+    def set_name(self, name):
+        self.__name = name
+
     # def set_animal_type()
+    def set_animal_type(self, animal_type):
+        self.__animal_type = animal_type
+
     # def set_age()
+    def set_age(self, age):
+        self.__age = age
+        
     # def get_name()
     # def get_animal_type()
     # def get_age()
