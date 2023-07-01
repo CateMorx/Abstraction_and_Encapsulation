3356485b6f12d789fa24cb7c770ec1671d0101f4
CateMorx <morco.catelynjoy.12192003@gmail.com>
2023-07-02 01:08:12 +0800

    creates GUI constructor, added label, text box, and button widget to GUI, copy pasted 2 more times for age and animal type

--- Pet_Object.py
+++ Pet_Object.py
@@ -5,10 +5,40 @@
 
 #imports necessary elements
 from Pet_Class import Pet
+import tkinter as tkinter
 
+pet=Pet()
 #Class Pet
-#GUI constructor
-# def for set_name()
-# def for set_animal_type()
-# def for set_age()
-# def for get_name(), get_animal_type(), get_age()
+class Pet_GUI:
+    #GUI constructor
+    def __init__(self, pet, GUI):
+        self.GUI = GUI
+        self.pet= pet
+
+        #Label, Text Box, and Button for name Input
+        self.pet.name_label = tkinter.Label(GUI, text="Please Enter the Name of Your Pet:")
+        self.pet.name_label.pack(pady=5)
+        self.pet.name_entry = tkinter.Entry(GUI)
+        self.pet.name_entry.pack(pady=5)
+        self.pet.name_button= tkinter.Button(GUI, text="Enter", command=self.name_enter)
+        self.pet.name_button.pack(pady=10)
+
+        #Label, Text Box, and Button for name Input
+        self.pet.name_label = tkinter.Label(GUI, text="Please Enter the Name of Your Pet:")
+        self.pet.name_label.pack(pady=5)
+        self.pet.name_entry = tkinter.Entry(GUI)
+        self.pet.name_entry.pack(pady=5)
+        self.pet.name_button= tkinter.Button(GUI, text="Enter", command=self.name_enter)
+        self.pet.name_button.pack(pady=10)
+
+        #Label, Text Box, and Button for name Input
+        self.pet.name_label = tkinter.Label(GUI, text="Please Enter the Name of Your Pet:")
+        self.pet.name_label.pack(pady=5)
+        self.pet.name_entry = tkinter.Entry(GUI)
+        self.pet.name_entry.pack(pady=5)
+        self.pet.name_button= tkinter.Button(GUI, text="Enter", command=self.name_enter)
+        self.pet.name_button.pack(pady=10)
+    # def for set_name()
+    # def for set_animal_type()
+    # def for set_age()
+    # def for get_name(), get_animal_type(), get_age()
