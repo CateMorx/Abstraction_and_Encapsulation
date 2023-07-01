56ac2c791844d11361cba5f5b6657ed93a5c5b55
CateMorx <morco.catelynjoy.12192003@gmail.com>
2023-07-01 13:31:31 +0800

    Class TestFan

--- Test_Fan.py
+++ Test_Fan.py
@@ -3,7 +3,12 @@
 # creates two Fan objects. For the first object, assign the maximum speed, radius 10, color yellow, and turn it on.
 # Assign medium speed, radius 5, color blue, and turn it off for the second object. Display each object’s speed, radius, color, and on properties.
 
+#Imports necessarry elements
+from Class_Fan import Fan
+from tkinter import Tk
+
 #Class TestFan
+class Test_Fan:
 #def for assigning values for fan 1 & 2
 #def GUI
 #starts the event loop of the GUI application
