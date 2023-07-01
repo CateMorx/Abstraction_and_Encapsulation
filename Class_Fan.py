2fd14ae12d6b9c74b3c5c1a8a4431c93bd053ff6
CateMorx <morco.catelynjoy.12192003@gmail.com>
2023-07-01 13:12:29 +0800

    Python file with pseudocode

---
+++ Class_Fan.py
@@ -0,0 +1,12 @@
+#Created by: Catelyn Joy M.Morco from BSCPE 1-4
+#Assignment 9
+#The Fan class represents a fan and has constants SLOW, MEDIUM, and FAST for speed values. It has private data fields for speed, on/off status, radius, and color. 
+#The class provides accessor and mutator methods for all data fields and a constructor to create a fan object with default values.
+
+#Class
+# A constructor that creates a fan with the speed (default SLOW), radius (default 5), color (default blue), and on (default False)
+
+#accessor and mutator for speed
+#accessor and mutator for on
+#accessor and mutator for radius
+#accessor and mutator for color
