a7866a7041ad2e3c477056f837cc4749a0ab937f
CateMorx <morco.catelynjoy.12192003@gmail.com>
2023-07-01 21:21:25 +0800

    Car Object python file

---
+++ Car_Object.py
@@ -0,0 +1,15 @@
+#Created by: Catelyn Joy M.Morco from BSCPE 1-4
+#Assignment 9
+#design a program that creates a Car object then calls the accelerate method five times. After each call to the accelerate method, 
+#get the current speed of the car and display it. Then call the brake method five times. After each call to the brake method, 
+#get the current speed of the car and display it.
+
+#Class for Car_Object
+
+    #GUI constructor
+
+    #def create car object
+        #accelerates and display speed 5 times
+        #decelerates and display speed 5 times
+
+    #def that creates rainbow window background
