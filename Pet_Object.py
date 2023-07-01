c37a62ce3e08188de0e37797789fdd4b159afa70
CateMorx <morco.catelynjoy.12192003@gmail.com>
2023-07-02 01:02:42 +0800

    python file for Pet_Object

---
+++ Pet_Object.py
@@ -0,0 +1,14 @@
+#Created by: Catelyn Joy M.Morco from BSCPE 1-4
+#Assignment 9
+#write a program that creates an object of the class and prompts the user to enter the name, type, and age of his or her pet. 
+#This data should be stored as the object’s attributes. Use the object’s accessor methods to retrieve the pet’s name, type, and age and display this data on the screen.
+
+#imports necessary elements
+from Pet_Class import Pet
+
+#Class Pet
+#GUI constructor
+# def for set_name()
+# def for set_animal_type()
+# def for set_age()
+# def for get_name(), get_animal_type(), get_age()
