dc5b77ad0e9a9f9869517cff0d905d394f1ce6c8
CateMorx <morco.catelynjoy.12192003@gmail.com>
2023-07-02 00:44:21 +0800

    third python program, pet class

---
+++ Pet_Class.py
@@ -0,0 +1,14 @@
+#Created by: Catelyn Joy M.Morco from BSCPE 1-4
+#Assignment 9
+#Write a class named Pet, which should have the following data attributes:_ _name, _ _animal_type, _ _age.
+# The Pet class should have an _ _init_ _ method that creates these attributes. It should also have the following methods:
+#set_name(), set_animal_type(), set_age(), get_name(), get_animal_type(), get_age()
+
+#Class Pet
+# Constructor with name, animal_type, age
+# def set_name()
+# def set_animal_type()
+# def set_age()
+# def get_name()
+# def get_animal_type()
+# def get_age()
